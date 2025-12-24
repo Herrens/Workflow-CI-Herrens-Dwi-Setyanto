d335d1d26905a655d55ce1f1081b8e286e851e0a
Herrens <herrensdwi@gmail.com>
2025-12-24 16:47:28 +0700

    fix mlflow run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import mlflow
+import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-
-mlflow.set_experiment("Telco-Churn-CI")
 
 df = pd.read_csv("data_clean/telco_clean.csv")
 
@@ -14,16 +13,16 @@ y = df[target_col]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
-)   
+)
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, model.predict(X_test))
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.save_model(model, "docker_model")
-    mlflow.sklearn.log_model(model, "model")
+acc = accuracy_score(y_test, model.predict(X_test))
 
-    print("CI Accuracy:", acc)
-    print("Model and metrics logged to MLflow.")    
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.save_model(model, "docker_model")
+mlflow.sklearn.log_model(model, "model")
+
+print("CI Accuracy:", acc)
+print("Model logged via MLflow Project.")
